1fc7114f50a3ee357e6e4b6c0c14ad1509b0317e
lindyem <lindy.merwin@gmail.com>
2020-11-12 10:42:49 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Global, css, jsx } from '@emotion/core'
+
+/**
+ * @function App
+ */
+const App = () => (
+  <>
+    <Global styles={GlobalCSS} />
+  </>
+)
+
+const GlobalCSS = css`
+  * {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html,
+  body,
+  .app {
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+
+
+    h1 {
+      color: white;
+    }
+  }
+  //
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0;
+    list-style: none;
+    padding: 0;
+  }
+`
+
+export default App
